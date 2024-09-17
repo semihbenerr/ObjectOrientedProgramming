14b5aab6eabc356f1d3c66956a848d1a4713673a
Semih BENER <benersemih2434@gmail.com>
2024-09-17 15:48:33 +0300

    Destructor

---
+++ 04_Destructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _04_Destructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            X();
+            GC.Collect();
+            Console.ReadLine();
+        }
+        static void X()
+        {
+            new MyClass();
+        }
+    }
+    class MyClass
+    {
+        public MyClass()//consturctor 
+        {
+            Console.WriteLine("Merhaba");
+        }
+        ~MyClass()
+        {
+            Console.WriteLine("Bye Bye");
+        }
+    }
+}
---
+++ 04_Destructor_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04_Destructor_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            while (a >= 1)
+            {
+                new MyClass(a--);
+            }
+            Console.WriteLine("*******************");
+            GC.Collect();
+
+            Console.ReadLine();
+        }
+    }
+    class MyClass
+    {
+        int no;
+        public MyClass(int no)
+        {
+            this.no = no;
+            Console.WriteLine($"{no}. nesne oluşturuldu");
+        }
+        ~MyClass()
+        {
+            Console.WriteLine($"{no}. imha edilecek");
+        }
+    }
+}
